ff389bebac839116391c797f097e7d79e2c1837f
chenyi <whale0827@icloud.com>
2023-02-06 11:56:34 +0100

    Feat #15: Implement EmailClient
    Added Email send function, test passed (but not automatic right now)

---
+++ src/main/java/se/kth/email/EmailClient.java
@@ -0,0 +1,65 @@
+package se.kth.email;
+
+import se.kth.github.StatusState;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class EmailClient {
+    private final String password;
+    public EmailClient() {
+        try {
+            this.password = Files.readString(Path.of("secrets/email_password"));
+        } catch (IOException e) {
+            System.err.println("No password file was found! Make sure you have a /secrets/email_password file");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendResultEmail(String receiver, StatusState state, String description){
+        // Sender's email ID
+        String from = "sefcicd.group4@gmail.com";
+        // Sending email through Gmails smtp
+        String host = "smtp.gmail.com";
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            // Set Subject: header field
+            message.setSubject("Commit Status changed to: " + state.toString());
+            // Now set the actual message
+            message.setText(description);
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
--- src/test/java/se/kth/ExampleTest.java
+++ src/test/java/se/kth/ExampleTest.java
@@ -1,6 +1,8 @@
 package se.kth;
 
 import org.junit.jupiter.api.Test;
+import se.kth.email.EmailClient;
+import se.kth.github.StatusState;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,5 +16,12 @@ public class ExampleTest {
     @Test
     public void testThatFails() {
         assertTrue(false);
+    }
+
+    // @Test
+    public void testSendEmail(){
+        var apiClient = new EmailClient();
+        apiClient.sendResultEmail("whale0827@icloud.com", StatusState.SUCCESS, "Commit check passed");
+        assertTrue(true);
     }
 }
