948fa5bd433caed2f2ff81a3975aeaadc97678ae
Marten Voorberg <martenvoorberg@gmail.com>
2023-02-06 15:45:27 +0100

    Commit with passing tests

--- src/test/java/se/kth/ExampleTest.java
+++ src/test/java/se/kth/ExampleTest.java
@@ -12,7 +12,7 @@ public class ExampleTest {
     }
 
     @Test
-    public void testThatFails() {
-        assertTrue(false);
+    public void testThatDoesNotFail() {
+        assertTrue(true);
     }
 }
