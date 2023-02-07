77c6a667d1aaa24c1faf2e65cde5da2a120458c8
David Ljunggren <davidxljunggren@gmail.com>
2023-02-07 21:36:02 +0100

    Fix #4 the commit status API now works as intended

--- src/main/java/se/kth/github/CommitStatus.java
+++ src/main/java/se/kth/github/CommitStatus.java
@@ -7,17 +7,29 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+import java.io.FileReader;
 import java.io.IOException;
+
+import java.util.Properties;
+
 
 public class CommitStatus implements GithubApiClient {
 
-    private String owner;
-    private String repo;
-    private String sha;
-    private String url;
+    private final String owner;
+    private final String repo;
+    private final String sha;
+    private final String url;
+    private String token;
 
+    /**
+     * Constructor for a commit status
+     *
+     * @param owner The owner of the repo
+     * @param repo The name of the repo
+     * @param sha The commit id
+     */
 
-    public CommitStatus(String owner, String repo, String sha, StatusState state) {
+    public CommitStatus(String owner, String repo, String sha) {
         this.owner = owner;
         this.repo = repo;
         this.sha = sha;
@@ -28,13 +40,25 @@ public class CommitStatus implements GithubApiClient {
     public void createOrUpdateCommitStatus(String commitSHA, StatusState state, String targetUrl, String description, String context) {
 
     }
-
+    /**
+     * Creates a POST request with the Github Access Token as header.
+     *
+     * @param state the status one wants to update or create for the commit
+     */
     @Override
     public CloseableHttpResponse postStatus(StatusState state) throws IOException {
 
+        try (FileReader reader = new FileReader("src\\main\\java\\se\\kth\\secrets\\github-token")) {
+            Properties properties = new Properties();
+            properties.load(reader);
+            token = properties.getProperty("token");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         JsonObject jo = new JsonObject(); // create json object
 
-        switch (state) {
+        switch (state) { //add the appropriate state as a property
             case SUCCESS -> jo.addProperty("state", "success");
 
             case ERROR -> jo.addProperty("state", "error");
@@ -44,11 +68,11 @@ public class CommitStatus implements GithubApiClient {
             case FAILURE -> jo.addProperty("state", "failure");
         }
 
-        try (final CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        try (final CloseableHttpClient httpClient = HttpClients.createDefault()) { //create http client and add headers
             HttpPost hp = new HttpPost(url);
 
             hp.addHeader("Accept", "application/vnd.github+json");
-            hp.addHeader("Authorization", "token " + "mockmock");
+            hp.addHeader("Authorization", "Bearer " + token);
 
             StringEntity se = new StringEntity(jo.toString());
 
