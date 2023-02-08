29855b61ccc947384674060ba34b6af7a28c29a0
KarlisV <59483828+KarlisKV@users.noreply.github.com>
2023-02-08 11:03:14 +0200

    Feat #6: Implement CommitWrapper and PushWrapper
    * #6 Implement initial version of JSONCommitWrapper and JSONPushWrapper
    
    * #6 Implement push and commit wrappers
    
    * #6 Implement push and commit wrappers and tests for commit wrapper
    
    * #6 Implement push and commit wrappers and tests for JSONCommitWrapper
    
    * Fix #6: Fix tests that broke due to additional CommitWrapper fields
    
    ---------
    
    Co-authored-by: Marten Voorberg <martenvoorberg@gmail.com>

--- src/main/java/se/kth/wrappers/JSONPushWrapper.java
+++ src/main/java/se/kth/wrappers/JSONPushWrapper.java
@@ -14,21 +14,36 @@ public class JSONPushWrapper implements PushWrapper {
     private final Map<String, Object> pushMap;
     private final List<CommitWrapper> commitWrappers;
 
+    /**
+     * @param request the request from the GitHub API
+     * @throws IOException if the request cannot be read
+     */
     public JSONPushWrapper(HttpServletRequest request) throws IOException {
-        BufferedReader reader = request.getReader();
-        String jsonString = reader.lines().collect(Collectors.joining());
-        ObjectMapper mapper = new ObjectMapper();
-        this.pushMap = mapper.readValue(jsonString, Map.class);
 
+        String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        ObjectMapper mapper = new ObjectMapper();
+        this.pushMap = mapper.readValue(body, Map.class);
         this.commitWrappers = new ArrayList<>();
-        ArrayList commits = (ArrayList) pushMap.get("commits");
-        for (int i = 0; i < commits.size(); i++) {
-            commitWrappers.add(new JSONCommitWrapper((Map<String, Object>) commits.get(i)));
+        List<Map<String, Object>> commits = (List<Map<String, Object>>) pushMap.get("commits");
+        for (Map<String, Object> commit : commits) {
+            commitWrappers.add(new JSONCommitWrapper(commit));
         }
 
     }
 
+    /**
+     * @return the list of commit wrappers
+     */
+    @Override
     public List<CommitWrapper> getCommitWrappers() {
         return this.commitWrappers;
     }
+
+    /**
+     * @return the map of the push object from the GitHub API
+     */
+    public Map<String, Object> getPushMap() {
+        return this.pushMap;
+    }
+
 }
