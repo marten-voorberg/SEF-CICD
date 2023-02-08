bb4f0a7883ff801d80556a8673c44a6360441411
鲸 <91104491+WhaleOMO@users.noreply.github.com>
2023-02-08 10:19:26 +0100

    Feat #15: Implement EmailClient for notification of CI results
    * Feat #15: Implement EmailClient
    
    Added Email send function, test passed (but not automatic right now)
    
    * Doc #15: Added JavaDoc
    
    * Misc #15: Remove dead test and unnecessary print statements
    
    ---------
    
    Co-authored-by: Marten Voorberg <martenvoorberg@gmail.com>

--- src/test/java/se/kth/ExampleTest.java
+++ src/test/java/se/kth/ExampleTest.java
@@ -1,6 +1,8 @@
 package se.kth;
 
 import org.junit.jupiter.api.Test;
+import se.kth.email.EmailClient;
+import se.kth.github.StatusState;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
